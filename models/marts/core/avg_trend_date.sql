34381586483da0d7a9e873785275eec0c5e0a788
Gery <gergana.kaymakanova@nimbusintelligence.com>
2024-04-01 18:03:36 +0200

    MODIFICATIONS

--- models/marts/core/avg_trend_date.sql
+++ models/marts/core/avg_trend_date.sql
@@ -1,6 +1,7 @@
 WITH avg_trend_date AS (
-    SELECT CAST(AVG(DATEDIFF('day', TO_TIMESTAMP(publishing_date, 'YYYY-MM-DD HH24:MI:SS.FF3'), TO_DATE(trending_date, 'YY.DD.MM'))) AS DECIMAL(10,2))
+    SELECT Country_Code, region_classification, CAST(AVG(DATEDIFF('day', TO_TIMESTAMP(publishing_date, 'YYYY-MM-DD HH24:MI:SS.FF3'), TO_DATE(trending_date, 'YY.DD.MM'))) AS DECIMAL(10,2))
     FROM {{ ref('model_hypehouse') }}
+    GROUP BY Country_Code, region_classification
 )
 
 SELECT * FROM avg_trend_date
--- models/marts/core/trending_videos.sql
+++ models/marts/core/trending_videos.sql
@@ -1,8 +1,8 @@
 with trending_videos as 
 
-(SELECT channel_title, COUNT(*) AS num_trending_videos
+(SELECT Country_Code, region_classification, channel_title, COUNT(*) AS num_trending_videos
 FROM {{ ref('model_hypehouse') }}
-GROUP BY Channel_Title
+GROUP BY Country_Code, region_classification, Channel_Title
 )
 
 select * from trending_videos
